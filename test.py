bce6e433c21f0a21cee0033c9106bdcebd32fbfd
Xinyang Huang <huangxinyang030218@126.com>
2025-06-18 16:01:51 +0100

    complete test.py's top_k fucntion

--- test.py
+++ test.py
@@ -18,12 +18,14 @@ parser.add_argument("--test_gt_path", type=str, required=True,
 parser.add_argument("--save_path", type=str, required=True,
                     help="path to save the predicted masks")
 parser.add_argument('--n_experts', type=int, default=4)
+parser.add_argument('--top_k', type=int, default=2)
+
 args = parser.parse_args()
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_loader = TestDataset(args.test_image_path, args.test_gt_path, 352)
-model = SAM2UNet(n_experts=args.n_experts).to(device)
+model = SAM2UNet(n_experts=args.n_experts, top_k=args.top_k).to(device)
 # model.load_state_dict(torch.load(args.checkpoint), strict=True)
 checkpoint = torch.load(args.checkpoint, map_location=device)
 if isinstance(checkpoint, dict) and 'model_state_dict' in checkpoint:
